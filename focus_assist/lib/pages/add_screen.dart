7480cc9f40f88d7fa994fc62124ce35d645752b0
UIT19522006 <UIT19522006@gm.uit.edu.vn>
2021-05-08 17:58:03 +0700

    Chỉnh sửa giao diện thêm mới activity

--- focus_assist/lib/pages/add_screen.dart
+++ focus_assist/lib/pages/add_screen.dart
@@ -113,139 +113,210 @@ class _AddNewState extends State<AddNew> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(0xff8A2BE2),
+          title: Text("Add new activity", style: TextStyle(fontSize: 25)),
+          actions: [
+            FlatButton(
+                onPressed: () {
+                  print("Fucjjj");
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(3.0),
+                  child: Text(
+                    "Create",
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                ))
+          ],
+          elevation: 1,
+        ),
         body: ListView(
-      shrinkWrap: true,
-      padding: EdgeInsets.all(20.0),
-      children: <Widget>[
-        SizedBox(
-          height: 30,
-        ),
-        // Tiêu đề
-        Center(
-            child: Container(
-          color: Colors.green,
-          // decoration: BoxDecoration(
-          //     border: Border.all(color: Colors.black)),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              'Add new Activity',
-              style: TextStyle(fontSize: 40, color: Colors.white),
-              textAlign: TextAlign.center,
+          shrinkWrap: true,
+          //padding: EdgeInsets.all(20.0),
+          children: <Widget>[
+            // Thêm tên của activity và các description
+            Container(
+                decoration: BoxDecoration(
+                    color: Color(0xff8A2BE2),
+                    borderRadius: BorderRadius.all(Radius.circular(0))),
+                // height: double.infinity,
+                //width: double.infinity,
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 10,
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Activity Name',
+                          labelStyle: TextStyle(color: Colors.white),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(4)),
+                            borderSide:
+                                BorderSide(width: 1, color: Colors.white),
+                          ),
+                          disabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(4)),
+                            borderSide:
+                                BorderSide(width: 1, color: Colors.white),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(4)),
+                            borderSide:
+                                BorderSide(width: 1, color: Colors.white),
+                          ),
+                          errorBorder: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(4)),
+                              borderSide:
+                                  BorderSide(width: 1, color: Colors.white)),
+                          focusedErrorBorder: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(4)),
+                              borderSide:
+                                  BorderSide(width: 1, color: Colors.white)),
+                        ),
+                        style: TextStyle(fontSize: 20, color: Colors.white),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      // Description
+                      TextField(
+                        decoration: InputDecoration(
+                            isDense: true,
+                            contentPadding: EdgeInsets.all(28),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(4)),
+                              borderSide:
+                                  BorderSide(width: 1, color: Colors.white),
+                            ),
+                            disabledBorder: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(4)),
+                              borderSide:
+                                  BorderSide(width: 1, color: Colors.white),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(4)),
+                              borderSide:
+                                  BorderSide(width: 1, color: Colors.white),
+                            ),
+                            errorBorder: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(4)),
+                                borderSide:
+                                    BorderSide(width: 1, color: Colors.white)),
+                            focusedErrorBorder: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(4)),
+                                borderSide:
+                                    BorderSide(width: 1, color: Colors.white)),
+                            border: OutlineInputBorder(),
+                            labelText: 'Description',
+                            labelStyle:
+                                TextStyle(color: Colors.white, fontSize: 20)),
+                        style: TextStyle(fontSize: 15, color: Colors.white),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                    ],
+                  ),
+                )),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 10, 10, 10),
+              child: Row(children: [
+                Text(
+                  "Start date: ",
+                  style: TextStyle(fontSize: 20),
+                ),
+                Text(
+                  startTime.toString().substring(0, 10),
+                  style: TextStyle(fontSize: 20),
+                ),
+                FlatButton.icon(
+                  icon: Icon(Icons.date_range),
+                  onPressed: () {
+                    showDatePicker(
+                            context: context,
+                            initialDate: startTime,
+                            firstDate: DateTime.utc(2020, 1, 1),
+                            lastDate: DateTime.utc(2120, 31, 12))
+                        .then((date) {
+                      setState(() {
+                        startTime = date;
+                      });
+                    });
+                  },
+                  label: Text(""),
+                )
+              ]),
             ),
-          ),
-        )),
-        SizedBox(
-          height: 10,
-        ),
-        // Thêm tên của activity
-        Padding(
-          padding: const EdgeInsets.fromLTRB(18.0, 0, 18.0, 0),
-          child: TextField(
-            decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Enter name of new activity'),
-            style: TextStyle(fontSize: 20),
-          ),
-        ),
-        SizedBox(
-          height: 10,
-        ),
-        // Description
-        Padding(
-          padding: const EdgeInsets.fromLTRB(18.0, 0, 18.0, 0),
-          child: TextField(
-            decoration: InputDecoration(
-                border: OutlineInputBorder(), hintText: 'Add description'),
-            style: TextStyle(fontSize: 15),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(20, 10, 10, 10),
-          child: Row(children: [
-            Text(
-              "Start date: ",
-              style: TextStyle(fontSize: 20),
+            Divider(
+              height: 10,
+              color: Colors.black54,
             ),
-            Text(
-              startTime.toString().substring(0, 10),
-              style: TextStyle(fontSize: 20),
+            // Bảng chọn loại của cái activity
+            Row(
+              children: [
+                SizedBox(
+                  width: 20,
+                ),
+                Text(
+                  "Select type",
+                  style: TextStyle(fontSize: 20),
+                ),
+                SizedBox(
+                  width: 30,
+                ),
+                DropdownButton<String>(
+                  value: dropDownValue,
+                  icon: const Icon(Icons.arrow_drop_down_outlined),
+                  iconSize: 24,
+                  elevation: 16,
+                  style:
+                      const TextStyle(color: Colors.deepPurple, fontSize: 20),
+                  underline: Container(
+                    height: 2,
+                    color: Colors.deepPurpleAccent,
+                  ),
+                  onChanged: (String newValue) {
+                    setState(() {
+                      dropDownValue = newValue;
+                    });
+                  },
+                  items: <String>['Fixed', 'Flexible', 'Repeating']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                )
+              ],
             ),
-            FlatButton.icon(
-              icon: Icon(Icons.date_range),
-              onPressed: () {
-                showDatePicker(
-                        context: context,
-                        initialDate: startTime,
-                        firstDate: DateTime.utc(2020, 1, 1),
-                        lastDate: DateTime.utc(2120, 31, 12))
-                    .then((date) {
-                  setState(() {
-                    startTime = date;
-                  });
-                });
-              },
-              label: Text(""),
-            )
-          ]),
-        ),
-        Divider(
-          height: 10,
-          color: Colors.black54,
-        ),
-        // Bảng chọn loại của cái activity
-        Row(
-          children: [
-            SizedBox(
-              width: 20,
-            ),
-            Text(
-              "Select type",
-              style: TextStyle(fontSize: 20),
-            ),
-            SizedBox(
-              width: 30,
-            ),
-            DropdownButton<String>(
-              value: dropDownValue,
-              icon: const Icon(Icons.arrow_drop_down_outlined),
-              iconSize: 24,
-              elevation: 16,
-              style: const TextStyle(color: Colors.deepPurple, fontSize: 20),
-              underline: Container(
-                height: 2,
-                color: Colors.deepPurpleAccent,
+            Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: (dropDownValue == 'Fixed')
+                    ? Fixed()
+                    : (dropDownValue == 'Flexible')
+                        ? Flexible()
+                        : Repeating()),
+            Center(
+              child: RaisedButton(
+                onPressed: () {},
+                child: Text("Add"),
               ),
-              onChanged: (String newValue) {
-                setState(() {
-                  dropDownValue = newValue;
-                });
-              },
-              items: <String>['Fixed', 'Flexible', 'Repeating']
-                  .map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
             )
           ],
-        ),
-        Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: (dropDownValue == 'Fixed')
-                ? Fixed()
-                : (dropDownValue == 'Flexible')
-                    ? Flexible()
-                    : Repeating()),
-        Center(
-          child: RaisedButton(
-            onPressed: () {},
-            child: Text("Add"),
-          ),
-        )
-      ],
-    ));
+        ));
   }
 }
